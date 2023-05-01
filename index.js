620a698d33389f5c3e4b2b994ebaa8f7a5bb93bf
safatsaat2 <sakibsafat47@gmail.com>
2023-05-02 01:11:06 +0600

    news json

--- index.js
+++ index.js
@@ -2,19 +2,41 @@ const express = require('express')
 const app = express()
 const port = 5000
 const cors = require('cors')
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json')
 
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Dragon is running')
+    res.send('Dragon is running')
 })
 
-app.get('/categories', (req, res) =>{
+app.get('/categories', (req, res) => {
     res.send(categories)
 })
 
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id == 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id)
+        res.send(categoryNews)
+    }
+
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
